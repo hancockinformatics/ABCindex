ed9f2d10d12c9a7e5f5370f7de1ba7bf75f55b8b
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-11-17 11:43:03 -0800

    Moved premade colour schemes for tile/dot plots into a separate object/script.

---
+++ R/preset.palettes.R
@@ -0,0 +1,72 @@
+preset.palettes <- list(
+  "YP" = c(
+    "#FFC107",
+    "#FFDE07",
+    "#FAE040",
+    "#FFF0B3",
+    "#E3E3E3",
+    "#F1BEF1",
+    "#C55AC5",
+    "#C55AC5"
+  ),
+  "YB" = c(
+    "#FFC107",
+    "#FFDE07",
+    "#FAE040",
+    "#FFF0B3",
+    "#E3E3E3",
+    "#A9D5FD",
+    "#6FC0FF",
+    "#6FC0FF"
+  ),
+  "OP" = c(
+    "#FF8800",
+    "#FFA000",
+    "#FFB638",
+    "#FAD28C",
+    "#E3E3E3",
+    "#F1BEF1",
+    "#C55AC5",
+    "#C55AC5"
+  ),
+  "RB" = c(
+    "#FF0000",
+    "#FF5000",
+    "#FF7E42",
+    "#F7BB9E",
+    "#E3E3E3",
+    "#A9D5FD",
+    "#6FC0FF",
+    "#6FC0FF"
+  ),
+  "PAN" = c(
+    "#FF005D",
+    "#FFD500",
+    "#FAE040",
+    "#FFF0B3",
+    "#E3E3E3",
+    "#A9D5FD",
+    "#6FC0FF",
+    "#6FC0FF"
+  ),
+  "SUN" = c(
+    "#FF6D00",
+    "#FFAE00",
+    "#FAE040",
+    "#FFF0B3",
+    "#E3E3E3",
+    "#FFC7FF",
+    "#D46AD4",
+    "#D46AD4"
+  ),
+  "BOB" = c(
+    "#D40000",
+    "#FF5000",
+    "#FAE040",
+    "#FFF0B3",
+    "#E3E3E3",
+    "#A9D5FD",
+    "#6FC0FF",
+    "#6FC0FF"
+  )
+)
