7615a3fbb46e4981ba7bf89f6830330b64e3fb0a
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-03-07 10:38:42 -0800

    Added fallback fonts.

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ suppressPackageStartupMessages({
 app_theme <- bs_theme(
   version = 5,
   bootswatch = "cosmo",
-  base_font = font_google("Inter"),
+  base_font = font_collection(font_google("Inter", local = FALSE), "Roboto", "sans-serif"),
   font_scale = 0.9,
   primary = "#cc002c",
   secondary = "#373a3c",
